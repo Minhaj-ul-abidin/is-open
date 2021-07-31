1dce2a41b0691ccf78e489fa6e1e1dc823bebe28
Minhaj-ul-abidin <minhaj3labidin@gmail.com>
2021-08-01 03:59:35 +0530

    add sign up api for user sign up, create tests for the signup

--- routes/auth/v1/auth.js
+++ routes/auth/v1/auth.js
@@ -1,10 +1,68 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+
 const apiResponse = require("../../../utils/apiResponse");
+const User = require("../../../models/User");
 // @route GET /api/auth/v1/__test
 // @desc Test route
 // @access PUBLIC
 router.get("/__test", (req, res) =>
   apiResponse.successResponse(res, "auth routes working :)")
 );
+
+// @route POST /api/auth/v1/signup
+// @desc Sign up route
+// @access PUBLIC
+router.post("/signup", async (req,res) => {
+  const { name, email, password, role } = req.body;
+  const emailRegex = new RegExp(email);
+
+  try { 
+    let user = await User.findOne({
+      email:{  $regex : emailRegex, $options : "i"},
+      password: {$exists : true} 
+    });
+    if (user) {
+      return res
+        .status(400)
+        .json({ errors: [{ msg: "User already exists" }] });
+    }
+    const allowedRoles = ["user", "admin"];
+    if (role && !allowedRoles.includes(role)) {
+      return res.status(400).json({
+        errors: [
+          { msg: "Only user and moderator role is allowed to signup" },
+        ],
+      });
+    }
+    const userRole = role ? role : "user";
+    const approved = role === "user" ? true : false;
+    user = new User({
+      name: name,
+      role: userRole,
+      approved: approved,
+      email,
+      password,
+    });
+
+    const salt = await bcrypt.genSalt(10);
+
+    user.password = await bcrypt.hash(password, salt);
+
+    user = await user.save();
+
+    return res
+      .status(201)
+      .json({ success: true, message: "Signup successfully :)" });
+
+  } catch (err) {
+    console.log(err);
+    return apiResponse.ErrorResponse(res, "Server Error");
+  }
+
+
+});
+
+
 module.exports = router;
--- tests/auth.spec.js
+++ tests/auth.spec.js
@@ -14,3 +14,16 @@ describe("Auth test route", () => {
     await request(app).get("/api/auth/v1/__test").expect(200);
   });
 });
+
+describe("Signup API", () => {
+  test("Should sign up for a user", async () => {
+    await request(app)
+      .post("/api/auth/v1/signup")
+      .send({
+        name: "Test user",
+        email: "test@test.com",
+        password: "test1234",
+      })
+      .expect(201);
+  });
+});
